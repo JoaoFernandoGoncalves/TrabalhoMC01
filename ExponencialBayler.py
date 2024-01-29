258054f1461512f1589fa4ced98f3cea34f1818a
Daniel Hiroshi Nouchi <83257010+Hinouda@users.noreply.github.com>
2024-01-29 15:00:22 -0300

    Codigo pra exponencial de bayler incompleto
    arrumar/tentar descobrir oq fazer com o e^r

---
+++ ExponencialBayler.py
@@ -0,0 +1,81 @@
+import math
+import matplotlib.pyplot
+
+
+ln2 = 0.6931471805599453
+
+def solve_equation_range(start, end, step):
+    results = []
+    x_values = [start + step * i for i in range(int((end - start) / step) + 1)]
+    
+
+    for x in x_values:
+    
+        n = math.ceil(x / ln2 - 0.5)
+        if n >= 0:
+            two_n = 1 << n
+        else:
+            two_n = 1.0 / (1 << -n)
+
+        # Valor de e^r ou só do e, eu não sei mais 
+        y = 1 + x * (1 + x * (0.5 + x * (1 / 6 + (1/ 24)*x)))
+       
+       #valor do r
+        r = (x - n * ln2) / 256
+
+        #Tentativa de extrair o valor do e da expresão e^r
+        # e = math.exp(r)
+
+        #Tentativa de extrair o valor do e da expresão e^r por radiciação(pensando que o valor de y = e^r)
+        # if r > 0:
+        #     y = y**(1/r)
+            
+        # else:
+        #     y = 1
+        
+        #achar o valor de e^x(pensando que y != e^r), usando propriedade de exponenciação para multiplicar o valor de r por 256.
+        # da overflow se tirar o (r*256) e deixar somente (y**256) - pensando que y = e^r e não y = e,
+        #sendo esse e diferente do valor de euler
+        result = two_n * (y ** (r *256))
+        results.append(result)
+    
+    return results
+
+#função para criar o intervalo do arrya [0,5] - step:0.05
+def array_valor(start, end, step):
+    x_values = [start + step * i for i in range(int((end - start) / step) + 1)]
+    return x_values
+
+start = 0
+end = 5
+step = 0.05
+solutions = solve_equation_range(start, end, step)
+array_valores = array_valor(start,end,step)
+print(solutions)
+# print("Solutions:", solutions)
+
+#função para calcular o e^x por função do python
+def calcular_elevado_a_x(array_valores):
+    resultados = []
+    for x in array_valores:
+        resultado = math.exp(x)
+        resultados.append(resultado)
+    return resultados
+
+#função para calcular a diferença dos resultados entre as funções python e as minhas
+def calcular_diferenca(array1, array2):
+    if len(array1) != len(array2):
+        raise ValueError("Os arrays têm tamanhos diferentes")
+
+    lista_erros = []
+    for valor1, valor2 in zip(array1, array2):
+        erro = valor1 - valor2
+        lista_erros.append(erro)
+    return lista_erros
+
+
+resultados = calcular_elevado_a_x(array_valores)
+print(resultados)
+lista_erros = calcular_diferenca(solutions,resultados)
+matplotlib.pyplot.plot(array_valores, lista_erros)
+matplotlib.pyplot.show()
